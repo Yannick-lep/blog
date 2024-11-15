f621284c7db19007a144dfbdc4c8d3838efa3ffe
yannick lepinay <you@example.comlepinayyannick7@gmail.com>
2024-11-15 11:45:15 +0100

    ajout de la librairie htmx

--- index.php
+++ index.php
@@ -27,8 +27,13 @@ $connexion = null;
 <body>
     <main>
         <header>
-            <a href="add_form.php">Ajouter un article</a>
+            <a href="add_form.php"
+            hx-get="add_form.php"
+            hx-target="#form_ajout_article"
+            hx-swap="outerHTML"
+            >Ajouter un article</a>
         </header>
+        <section id="form_ajout_article"></section>
         <?php
         foreach($articles as $article):
         
