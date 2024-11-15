ecb1430a4d91bd4f8628da91e9a70eaad43809b7
yannick lepinay <you@example.comlepinayyannick7@gmail.com>
2024-11-15 11:35:05 +0100

    initialisation du projet

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ $connexion = null;
     <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+  <script src="https://unpkg.com/htmx.org@2.0.3"></script>
 </head>
 <body>
     <main>
