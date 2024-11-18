fbbf3f61da2895497855b2e792af2862a066eea7
yannick lepinay <you@example.comlepinayyannick7@gmail.com>
2024-11-18 10:09:04 +0100

    ajout de faker php avec composer

--- src/login.php
+++ src/login.php
@@ -1,14 +1,20 @@
+<?php
+require 'verif_login.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
 </head>
+
 <body>
     <main>
 <h1>Se connecter</h1>
-<form method="POST" class="formlogin">
+<form method="POST" target="_self" class="formlogin">
     <div>
         <input type="email" name="email" required placeholder="Adresse email">
     </div>
