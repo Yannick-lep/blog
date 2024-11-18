f173cf7b1cf5509fdd581be50a33e4af5217510c
yannick lepinay <you@example.comlepinayyannick7@gmail.com>
2024-11-18 09:56:28 +0100

    ajout- de composer faker php

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <main>
+<h1>Se connecter</h1>
+<form method="POST" class="formlogin">
+    <div>
+        <input type="email" name="email" required placeholder="Adresse email">
+    </div>
+    <div>
+        <input type="password" name="password" required placeholder="Mot de passe">
+    </div>
+    <div>
+        <input type="submit" value="Se connecter">
+    </div>
+</form>
+</main>
+</body>
+</html>
+
